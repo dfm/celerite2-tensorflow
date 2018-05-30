998d000c60b397ee5e6e48a5cb821386f7b5073a
Dan Foreman-Mackey <danfm@nyu.edu>
2018-05-30 12:07:03 -0400

    fixing #1

--- celeriteflow/__init__.py
+++ celeriteflow/__init__.py
@@ -3,7 +3,13 @@
 from __future__ import division, print_function
 __version__ = "0.0.1"
 
-__all__ = ["CeleriteFlowMatrix", "to_dense", "get_matrices", "factor", "solve"]
+try:
+    __CELERITEFLOW_SETUP__
+except NameError:
+    __CELERITEFLOW_SETUP__ = False
 
-from .celeriteflow import CeleriteFlowMatrix
-from .ops import to_dense, get_matrices, factor, solve
+if not __CELERITEFLOW_SETUP__:
+    __all__ = ["CeleriteFlowMatrix", "to_dense", "get_matrices", "factor", "solve"]
+
+    from .celeriteflow import CeleriteFlowMatrix
+    from .ops import to_dense, get_matrices, factor, solve
